bc823632cea9f000ae7ee5ccec9bad6774ff5ec8
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2025-10-05 15:06:39 +0530

    style: update font, adjust paddings & gaps for article section

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
@@ -10,4 +10,13 @@ export default defineConfig({
   site: 'https://prnsh.site',
   integrations: [mdx(), sitemap()],
   vite: { plugins: [tailwindcss()], },
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Doto",
+      weights: [900],
+      cssVariable: "--font-pixel"
+    }]
+  }
 });
+
